7445a2249f312c17bc262412b443192b69e4b30f
rmarcu <roman.marcu@crfhealth.com>
2018-02-12 10:16:19 +0200

    IU-2017.1.3 <rmarcu@rmarcu840g3 Create ide.general.xml, git.xml, project.default.xml, Enum.java, Interface.java, other.xml, Default.xml, find.xml, ui.lnf.xml, lombok.xml, module-info.java, filetypes.xml, projectView.xml, package-info.java, gradle.run.settings.xml, code.style.schemes.xml, plugin_ui.xml, gerrit_settings.xml, databaseDrivers.xml, mavenVersion.xml, AnnotationType.java, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, Default copy.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, debugger.xml, sonarlint.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017 CRF Box Oy. All Rights Reserved. Company Confidential.
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author CRF Health
+ */ 
+public class ${NAME} {
+}
---
+++ fileTemplates/internal/module-info.java
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2017 CRF Box Oy. All Rights Reserved. Company Confidential.
+ */
+#parse("File Header.java")
+module #[[$MODULE_NAME$]]# {
+}
